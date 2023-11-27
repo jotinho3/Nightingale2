91b6860b27a4e4c72835126492d1080602ff2363
João Pedro Marques Chaves <jchaves@digitalhouse.com>
2023-11-27 18:09:15 -0300

    Aula 27_11_2023

--- js/Carrousel.js
+++ js/Carrousel.js
@@ -1,11 +1,13 @@
-var slides = document.querySelectorAll('.slide');
-var currentSlide = 0;
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0; //variavel de contador
 
 function nextSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.remove('active'); //remover o active da imagem que esta mostrando
+  currentSlide = (currentSlide + 1) % slides.length; 
   slides[currentSlide].classList.add('active');
 }
 
 setInterval(nextSlide, 3000);
+
+
 
